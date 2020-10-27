8ebfc7e33853e800499be551d5f3aca9e2fb5fd4
jahnellybean <jmadarang87@gmail.com>
2020-10-27 00:27:31 -0500

    refactored

--- js/script.js
+++ js/script.js
@@ -3,25 +3,8 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-// list parameter to represent student data and pageParameter to represent page number that will be passed
-
-
-// defines how many students per page
+// defines how many students per page to list
 const studentsPerPage = 9;
-
 
 // creates element and sets property
 function createElement( elementName, property, value ) {
@@ -29,116 +12,107 @@ function createElement( elementName, property, value ) {
    element[property] = value;
    return element;
 }
-// appends child(ren) to parent
+// appends child(ren) to parent node
 function append( elementName, array ) {
    for ( let i = 0; i < array.length; i++ ) {
       elementName.appendChild(array[i])
    }
 }
 
-// creates showPage function to create and insert/append elements needed to display a "page" of nine students
+/* showPage function creates and inserts/appends elements needed to display a "page" of nine students
+the function takes 2 parameters: 
+   list - represents student data
+   pageParameter - the selected page number
+*/
 function showPage( list, pageParameter ) {
+ // defines start and end indexes for list items to be displayed on given page
  const startIndex = ( pageParameter * studentsPerPage ) - studentsPerPage;
  const endIndex = ( pageParameter * studentsPerPage) - 1;
  const studentList = document.querySelector('.student-list');
  studentList.innerHTML = '';
+
  for ( let i = 0; i < list.length; i++ ) {
    if ( i >= startIndex && i <= endIndex ) {
-      // creates li
+      // creates new elements
       let li = createElement('li', 'className', 'student-item cf');
-
-      // creates image
       let img = createElement('img', 'className', 'avatar');
-      img.src = `${list[i].picture.large}`;
-      img.alt = `Profile Picture`;
-
-      // creates h3 with first + last name
       let h3 = document.createElement('h3');
-      let firstName = list[i].name.first;
-      let lastName = list[i].name.last;
-      h3.innerHTML = `${firstName} ${lastName}`;
-
-      //creates email span for email
       let spanEmail = createElement('spanEmail', 'className', 'email');
-      spanEmail.innerHTML = `${list[i].email}`;
-
-      // creates student div with img, h3 and email span
       let divStudent = createElement('div', 'className', 'student-details');
-      append(divStudent, [img, h3, spanEmail]);
-
-      // creates joined div for joined date
       let divJoined = createElement('div', 'className', 'joined-details');
       let spanDate = createElement('span', 'className', 'date');
-      spanDate.innerHTML = `Joined ${list[i].registered.date}`
+      let firstName = list[i].name.first;
+      let lastName = list[i].name.last;
+
+      // assigns additional properties
+      img.src = `${list[i].picture.large}`;
+      img.alt = `Profile Picture`;
+      h3.innerHTML = `${firstName} ${lastName}`;
+      spanEmail.innerHTML = `${list[i].email}`;
+      spanDate.innerHTML = `Joined ${list[i].registered.date}`;
+
+      // appends child[ren] to parents
+      append(divStudent, [img, h3, spanEmail]);
       append(divJoined, [spanDate]);
-
-      // appends student and joined div to li
       append(li, [divStudent, divJoined]);
-      
-      // appends li to main student list
       append(studentList, [li]);
-
    }
-
  }
 }
 
-
 /*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
+create the addPagination function
+this function will create and insert/append the elements needed for the pagination buttons
 */
 function addPagination( list ) {
    const pages = Math.ceil( list.length / studentsPerPage );
    const linkList = document.querySelector('.link-list');
    linkList.innerHTML = '';
    for ( let i = 1; i <= pages; i++ ) {
-      const li = document.createElement('li');
-      const button = document.createElement('button');
+      //creates new elements
+      let li = document.createElement('li');
+      let button = document.createElement('button');
+
+      // sets additonal properties
       button.type = 'button';
       button.textContent = i;
-      li.appendChild(button);
-      linkList.appendChild(li);
+
+      // appends child[ren] to parents
+      append(li, [button]);
+      append(linkList, [li]);
+
    }
- 
+   // adds click event listener to page buttons
    linkList.addEventListener( 'click', (e) => {
       if ( e.target.tagName == 'BUTTON') {
          const buttonTarget = e.target;
          const newButtons = document.querySelectorAll('button');
          const newPage = parseInt(buttonTarget.textContent);
          for ( let i = 0; i < newButtons.length; i++ ) {
+            // clears all button class names
             newButtons[i].className = '';
          }
-         buttonTarget.className = 'active';
-         showPage( list, newPage);
+            // sets clicked button class name to 'active'
+            buttonTarget.className = 'active';
+            showPage( list, newPage);
       }
    })
 }
 
-
-// Call functions
-showPage( data , 1 );
-addPagination( data );
-
-// add Search Component
+// creates search component
 const header = document.querySelector('header');
-const label = document.createElement('label');
-const input = document.createElement('input');
-const buttonSearch = document.createElement('button');
-const img = document.createElement('img');
-input.id = 'search';
-input.placeholder='Search by name...';
-buttonSearch.type = 'button';
-img.src = "img/icn-search.svg";
-img.alt = "Search icon";
-label.type = "search";
+let label = createElement('label', 'type', 'search');
+let input = createElement('input', 'id', 'search');
+let buttonSearch = createElement('button', 'type', 'button');
+let img = createElement('img', 'src', 'img/icn-search.svg');
 label.className = "student-search";
-buttonSearch.appendChild(img);
-label.appendChild(input);
-label.appendChild(buttonSearch);
-header.appendChild(label);
+input.placeholder='Search by name...';
+img.alt = "Search icon";
+append(buttonSearch, [img]);
+append(label, [input, buttonSearch]);
+append(header, [label]);
 
-// Search feature
+// keyup event listener for search
 input.addEventListener( 'keyup', (e) => {
    let inputSearch = input.value;
    let searchResult = [];
@@ -149,18 +123,18 @@ input.addEventListener( 'keyup', (e) => {
       if ( dataFirstLower.includes(searchLower)  || dataLastLower.includes(searchLower)) {
          searchResult.push(data[i]);
       } 
+      // adds pagination for search results
+      showPage( searchResult, 1 );
+      addPagination( searchResult );
    }  
-   showPage(searchResult, 1);
-   addPagination( searchResult );
+   // if no search matches
    if ( searchResult.length < 1 ) {
       const studentList = document.querySelector('.student-list');
       studentList.innerHTML = '';
       studentList.innerHTML = 'Sorry, no results found.';
    }
+})
 
-
-   } 
-)
-
-
-
+// call functions at beginning page load
+showPage( data , 1 );
+addPagination( data );
